264a119295f4eaa595386420ffb42b10a65aceac
Csaba Trucza <csaba.trucza@gmail.com>
2016-01-02 09:49:33 +0200

    Separated prediction from segmentation

--- HelloOpenCV/PredictorTransformation.h
+++ HelloOpenCV/PredictorTransformation.h
@@ -1,10 +1,54 @@
 #pragma once
 #include "SegmentedTransformation.h"
 
-class PredictorTransformation: public SegmentedTransformation
+class PredictorSegment
+{
+private:
+	inline std::vector<uchar> to_vector(const cv::Mat& mat)
+	{
+		std::vector<uchar> result;
+		if (mat.isContinuous()) {
+			result.assign(mat.datastart, mat.dataend);
+		}
+		else {
+			for (int i = 0; i < mat.rows; ++i) {
+				result.insert(result.end(), mat.ptr<uchar>(i), mat.ptr<uchar>(i) + mat.cols);
+			}
+		}
+		return result;
+	}
+
+	mutable std::vector<uchar> last;
+	mutable std::map<std::vector<uchar>, cv::Mat> m;
+public:
+	cv::Rect rect;
+	cv::Mat mat;
+
+	PredictorSegment(const cv::Rect& rect)
+		:rect(rect)
+	{}
+
+	void predict()
+	{
+		auto copy = to_vector(mat);
+
+		m[last] = mat;
+		last = copy;
+
+		auto result = m.find(copy);
+		if (result == m.end())
+			mat = 0;
+		else
+			mat = result->second;
+	}
+};
+
+//std::map<std::vector<uchar>, cv::Mat> PredictorSegment::m;
+
+class PredictorTransformation: public SegmentedTransformation<PredictorSegment>
 {
 protected:
-    void transform_segment(Segment& segment) const override 
+    void transform_segment(PredictorSegment& segment) const override 
     {
         segment.predict();
     }
--- HelloOpenCV/SegmentedTransformation.h
+++ HelloOpenCV/SegmentedTransformation.h
@@ -5,68 +5,35 @@
 
 class Segment
 {
-private:
-    inline std::vector<uchar> to_vector(const cv::Mat& mat)
-    {
-        std::vector<uchar> result;
-        if (mat.isContinuous()) {
-            result.assign(mat.datastart, mat.dataend);
-        }
-        else {
-            for (int i = 0; i < mat.rows; ++i) {
-                result.insert(result.end(), mat.ptr<uchar>(i), mat.ptr<uchar>(i) + mat.cols);
-            }
-        }
-        return result;
-    }
-
-    mutable std::vector<uchar> last;
-    static std::map<std::vector<uchar>, cv::Mat> m;
 public:
     cv::Rect rect;
     cv::Mat mat;
-public:
+
     Segment(const cv::Rect& rect)
         :rect(rect)
     {}
-
-    void predict()
-    {
-        auto copy = to_vector(mat);
-
-        m[last] = mat;
-        last = copy;
-
-        auto result = m.find(copy);
-        if (result == m.end())
-            mat = 0;
-        else
-            mat = result->second;
-
-    }
 };
 
-std::map<std::vector<uchar>, cv::Mat> Segment::m;
-
+template <class SegmentType=Segment>
 class SegmentedTransformation : public VideoTransformation
 {
 private:
-    mutable std::vector<Segment*> segments;
+    mutable std::vector<SegmentType> segments;
 
 public:
     cv::Mat transform(const cv::Mat& frame) const override {
         cv::Mat result = frame.clone();
-        for (std::vector<Segment*>::iterator i = segments.begin(); i != segments.end(); ++i)
+        for (typename std::vector<SegmentType>::iterator i = segments.begin(); i != segments.end(); ++i)
         {
-            cv::Mat roi = result((*i)->rect);
-            (*i)->mat = roi;
-            transform_segment(**i);
+            cv::Mat roi = result(i->rect);
+            i->mat = roi;
+            transform_segment(*i);
         }
         return result;
     }
 
 protected:
-    virtual void transform_segment(Segment& segment) const = 0;
+    virtual void transform_segment(SegmentType& segment) const = 0;
 
 public:
     SegmentedTransformation(int width, int height, int segment_width, int segment_height)
@@ -74,11 +41,14 @@ public:
         Segmentation segmentation(segment_width, segment_height);
         auto rects = segmentation.get_segments(width, height);
         for (auto i = rects.begin(); i != rects.end(); ++i)
-            segments.push_back(new Segment(*i));
+        {
+			SegmentType segment(*i);
+			segments.push_back(segment);
+        }
     }
 };
 
-class AveragingTransformation : public SegmentedTransformation
+class AveragingTransformation : public SegmentedTransformation<>
 {
 protected:
     void transform_segment(Segment& segment) const override
